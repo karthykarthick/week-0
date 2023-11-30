1283d7f3b3cb1645b18f4d0828f4a0bbcdf78358
karthick <karthylock@gmail.com>
2023-11-30 19:19:19 +0530

    added js functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+// function calc(){
+    
+//     let amount = document.querySelector('.principle')
+//     let time = document.querySelector('.time')
+//     let intrest = document.querySelector('.rate')
+//     let res = document.querySelector('.res')
+    
+//     let amount_val = amount.value 
+//     let time_val =  time.value
+//     let intrest_val =  intrest.value
+    
+
+//    console.log((amount_val * time_val * intrest_val)/100);
+//    let si = (amount_val * time_val * intrest_val)/100
+//    res.innerText = si
+//    document.body.appendChild(res)
+
+// }
